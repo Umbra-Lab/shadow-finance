41d85f9c8b455a3b27b5d3fe49148191b5e02e35
Armin-FalDiS <armin.fallah96@gmail.com>
2023-12-08 11:20:02 +0330

    Implement execute script

---
+++ execute.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/.env
+
+PROGRAM=$(python3 -c "import json; print(json.load(open('build/program.json'))['program'])")
+
+snarkos developer execute -p $PRIVATE_KEY -q $TESTNET -b $TESTNET/testnet3/transaction/broadcast --priority-fee 0 $PROGRAM "$@"
