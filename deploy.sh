b2cdb780d271718813abc517669e859ed890ec27
Armin-FalDiS <armin.fallah96@gmail.com>
2023-12-08 11:20:14 +0330

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-getopts ":d" dev
+rm -rf build
+leo run
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/.env
 
-if [ $dev = "d" ]; then
-    snarkos developer deploy --private-key $PRIVATE_KEY --query http://127.0.0.1:3033/ --priority-fee 0 --broadcast http://127.0.0.1:3033/testnet3/transaction/broadcast --path build $1
-else
-    snarkos developer deploy --private-key $PRIVATE_KEY --query $TESTNET --priority-fee 0 --broadcast $TESTNET/testnet3/transaction/broadcast --path build $1
-fi
+PROGRAM=$(python3 -c "import json; print(json.load(open('build/program.json'))['program'])")
+
+snarkos developer deploy -p $PRIVATE_KEY -q $TESTNET -b $TESTNET/testnet3/transaction/broadcast --priority-fee 0 --path build $PROGRAM
