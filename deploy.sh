bb5b47d2f050cb2e47603fb11fbfb6e359b3d0af
Armin-FalDiS <armin.fallah96@gmail.com>
2024-06-30 20:40:12 +0200

    Add priority fee to snarkos scripts

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ source $SCRIPT_DIR/.env
 
 PROGRAM=$(python3 -c "import json; print(json.load(open('build/program.json'))['program'])")
 
-snarkos developer deploy --network 1 --private-key $PRIVATE_KEY --query $NODE_URL --broadcast "$NODE_URL/testnet/transaction/broadcast" --path build $PROGRAM
+snarkos developer deploy --network 1 --private-key $PRIVATE_KEY --priority-fee 0 --query $NODE_URL --broadcast "$NODE_URL/testnet/transaction/broadcast" --path build $PROGRAM
--- execute.sh
+++ execute.sh
@@ -5,4 +5,4 @@ source $SCRIPT_DIR/.env
 
 PROGRAM=$(python3 -c "import json; print(json.load(open('build/program.json'))['program'])")
 
-snarkos developer execute --network 1 --private-key $PRIVATE_KEY --query $NODE_URL --broadcast $NODE_URL/testnet/transaction/broadcast $PROGRAM "$@"
+snarkos developer execute --network 1 --private-key $PRIVATE_KEY --priority-fee 0 --query $NODE_URL --broadcast $NODE_URL/testnet/transaction/broadcast $PROGRAM "$@"
